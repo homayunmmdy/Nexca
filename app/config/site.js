1e13238a49c08e2f560d07e9cc846e0c68e28343
Homayoun <homayoun763@gmail.com>
2024-04-07 20:47:17 +0330

    add project

---
+++ app/config/site.js
@@ -0,0 +1,8 @@
+const SiteConfig = {
+  name: "سایت خبری",
+  description: "با ما باشید با جدیدترین اخبار روز دنیا",
+  lang: "fa",
+  dir: "rtl",
+};
+
+export default SiteConfig;
---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import { Inter } from "next/font/google";
+import "./Font.css";
+import "./globals.css";
+import SiteConfig from "@/app/config/site"
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: `${SiteConfig.name} `,
+  description: SiteConfig.description,
+};
+
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang={SiteConfig.lang} dir={SiteConfig.dir}>
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
