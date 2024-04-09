29c8f84280160fc7f18973d8648373cf7fe5a028
Homayoun <homayoun763@gmail.com>
2024-04-09 21:02:45 +0330

    News page

--- app/(pages)/Posts/[id]/page.jsx
+++ app/(pages)/Posts/[id]/page.jsx
@@ -1,8 +1,10 @@
 "use client";
-import BreadCrump from "@/app/components/BreadCrump";
 import PostSkelton from "./PostSkelton";
-import NewsletterBox from "@/app/components/NewsletterBox";
 import SinglePost from "@/app/components/SinglePost";
+import VerticalNews from "@/app/components/sections/VerticalNews";
+import LinearNews from "@/app/components/sections/LinearNews";
+import ListAds from "@/app/components/ads/ListAds";
+
 const PostPage = () => {
   const post = SinglePost();
 
@@ -12,9 +14,13 @@ const PostPage = () => {
 
   return (
     <>
-      <div className="grid grid-cols-12 gap-4 p-4 md:p-8">
-        <div className="col-span-12 md:col-span-8 lg:col-span-9">
-          <img
+        <div className='px-6 sm:px-12'>
+
+    <VerticalNews secid="18" />
+    <div className="grid grid-cols-12 gap-4">
+        <div className="col-span-12 md:col-span-6">
+        <h1 className="text-center mb-3">{post.title}</h1>
+        <img
             className="w-full p-3 aspect-video rounded-3xl"
             src={post.imgurl}
             title={post.title}
@@ -23,18 +29,21 @@ const PostPage = () => {
             height="450"
             loading="lazy"
           />
-          <BreadCrump />
-          <div >
-            <h1 className="text-center mb-3">{post.title}</h1>
-            <p>
+              
+              <p>
               {post.description}
             </p>
             <p>{post.body}</p>
-          </div>
         </div>
-        <div className="col-span-12 md:col-span-4 lg:col-span-3">
-          {/* <NewsletterBox /> */}
+        <div className="col-span-12 md:col-span-3">
+          <LinearNews secid="2" />
+          <LinearNews secid="19" />
         </div>
+        <div className="col-span-12 md:col-span-3">
+         <ListAds />
+        </div>
+      </div>
+      
       </div>
     </>
   );
