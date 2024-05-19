0865fa504c7ce75c4119741fadcc1cc0a68130a0
Homayoun <homayoun763@gmail.com>
2024-05-19 23:43:38 +0330

    Services

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './components/Hero'
+import Services from './components/Services'
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Services />
     </div>
   )
 }
