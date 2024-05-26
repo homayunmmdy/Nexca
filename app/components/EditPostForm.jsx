94375c2dd0fd5312d66e83c238aaff9554a07104
Homayoun <homayoun763@gmail.com>
2024-05-26 23:59:34 +0330

    Refactor EditPostForm

--- app/components/EditPostForm.jsx
+++ app/components/EditPostForm.jsx
@@ -1,181 +1,70 @@
 "use client";
 import { useRouter } from "next/navigation";
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState } from "react";
+import FormField from "./FormField";
+import SelectField from "./SelectField";
+import useFetch from "@/app/hooks/useFetch";
 
 const EditPostForm = ({ ticket }) => {
-  const EDITMODE = ticket._id === "new" ? false : true;
+  const EDITMODE = ticket._id !== "new";
   const router = useRouter();
   const startingTicketData = {
-    title: "",
-    description: "",
-    body: "",
-    section: "1",
-    service: "1",
-    imgurl: "",
+    title: EDITMODE ? ticket.title : "",
+    description: EDITMODE ? ticket.description : "",
+    body: EDITMODE ? ticket.body : "",
+    section: EDITMODE ? ticket.section : "1",
+    service: EDITMODE ? ticket.service : "1",
+    imgurl: EDITMODE ? ticket.imgurl : "",
   };
 
-  if (EDITMODE) {
-    startingTicketData["title"] = ticket.title;
-    startingTicketData["description"] = ticket.description;
-    startingTicketData["body"] = ticket.body;
-    startingTicketData["service"] = ticket.service;
-    startingTicketData["section"] = ticket.section;
-    startingTicketData["imgurl"] = ticket.imgurl;
-  }
-
   const [formData, setFormData] = useState(startingTicketData);
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
-    const value = e.target.value;
-    const name = e.target.name;
-
-    setFormData((preState) => ({
-      ...preState,
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
       [name]: value,
     }));
   };
-  const [loading, setLoading] = useState(false)
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    if (EDITMODE) {
-      const res = await fetch(`/api/Posts/${ticket._id}`, {
-        method: "PUT",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify({ formData }),
-      });
-      if (!res.ok) {
-        throw new Error("Failed to update ticket");
-      }
-    } else {
-      const res = await fetch("/api/Posts", {
-        method: "POST",
-        body: JSON.stringify({ formData }),
-        //@ts-ignore
-        "Content-Type": "application/json",
-      });
-      if (!res.ok) {
-        throw new Error("Failed to create ticket");
-      }
+    const url = EDITMODE ? `/api/Posts/${ticket._id}` : "/api/Posts";
+    const method = EDITMODE ? "PUT" : "POST";
+    const headers = { "Content-Type": "application/json" };
+
+    const res = await fetch(url, {
+      method,
+      headers,
+      body: JSON.stringify({ formData }),
+    });
+
+    if (!res.ok) {
+      setLoading(false);
+      throw new Error(`Failed to ${EDITMODE ? "update" : "create"} ticket`);
     }
 
     router.refresh();
     router.push("/admin/posts");
   };
-  const [services, setServices] = useState();
 
-  useEffect(() => {
-    const fetchServices = async () => {
-      try {
-        const response = await axios.get(`/api/Services`);
-        setServices(response.data.data);
-      } catch (error) {
-        console.error("Error fetching services:", error);
-      }
-    };
-
-    fetchServices();
-  }, []);
-
-  const [sections, setSections] = useState();
-
-  useEffect(() => {
-    const fetchSections = async () => {
-      try {
-        const response = await axios.get(`/api/Section`);
-        setSections(response.data.data);
-      } catch (error) {
-        console.error("Error fetching sections:", error);
-      }
-    };
-
-    fetchSections();
-  }, []);
+  const { data: services } = useFetch("/api/Services");
+  const { data: sections } = useFetch("/api/Section");
 
   return (
-    <div className=" flex justify-center">
+    <div className="flex justify-center">
       {loading && <span className="absolute loading loading-ring loading-lg"></span>}
-      <form
-        onSubmit={handleSubmit}
-        method="post"
-        className="flex flex-col gap-3 w-full md:w-1/2 mb-3"
-      >
-        <h3 className="text-center font-semibold	text-2xl">{EDITMODE ? "Edit Post" : "New Post"}</h3>
-        <label>Image Link</label>
-        <input
-          id="imgurl"
-          name="imgurl"
-          type="text"
-          onChange={handleChange}
-          value={formData.imgurl}
-          className="input input-bordered input-primary w-full"
-        />
-        <label>title</label>
-        <input
-          id="title"
-          name="title"
-          type="text"
-          onChange={handleChange}
-          required={true}
-          value={formData.title}
-          className="input input-bordered input-primary w-full"
-        />
-        <label>lead</label>
-        <textarea
-          id="description"
-          name="description"
-          onChange={handleChange}
-          value={formData.description}
-          rows="5"
-          className="textarea textarea-primary"
-        />
-        <label>body</label>
-        <textarea
-          id="body"
-          name="body"
-          onChange={handleChange}
-          required={true}
-          value={formData.body}
-          rows="10"
-          className="textarea textarea-primary"
-        />
-          <label>Services</label>
-        <select
-          className="select select-primary w-full"
-          id="service"
-          name="service"
-          value={formData.service}
-          onChange={handleChange}
-        >
-          {services?.map((service) => (
-            <option key={service._id} value={service.secid}>
-              {service.name}
-            </option>
-          ))}
-        </select>
-        <label>Section</label>
-        <select
-          className="select select-primary w-full"
-          id="section"
-          name="section"
-          value={formData.section}
-          onChange={handleChange}
-        >
-          {sections?.map((section) => (
-            <option key={section._id} value={section.secid}>
-              {section.name}
-            </option>
-          ))}
-        </select>
-        <input
-          type="submit"
-          className="btn btn-active btn-primary"
-          value={EDITMODE ? "Save" : "Post"}
-        />
+      <form onSubmit={handleSubmit} method="post" className="flex flex-col gap-3 w-full md:w-1/2 mb-3">
+        <h3 className="text-center font-semibold text-2xl">{EDITMODE ? "Edit Post" : "New Post"}</h3>
+        <FormField id="imgurl" name="imgurl" label="Image Link" value={formData.imgurl} onChange={handleChange} />
+        <FormField id="title" name="title" label="Title" value={formData.title} onChange={handleChange} required />
+        <FormField id="description" name="description" type="textarea" label="Lead" value={formData.description} onChange={handleChange} />
+        <FormField id="body" name="body" type="textarea" label="Body" value={formData.body} onChange={handleChange} required />
+        {services && <SelectField id="service" name="service" label="Services" value={formData.service} onChange={handleChange} options={services} />}
+        {sections && <SelectField id="section" name="section" label="Section" value={formData.section} onChange={handleChange} options={sections} />}
+        <input type="submit" className="btn btn-active btn-primary" value={EDITMODE ? "Save" : "Post"} />
       </form>
     </div>
   );
--- app/components/HappyCustomer.jsx
+++ app/components/HappyCustomer.jsx
@@ -4,36 +4,36 @@ import Image from 'next/image'
 import Link from 'next/link'
 const HappyCustomer = () => {
   return (
-    <div><div class="w-full px-4 pt-16 pb-16" id="faq">
-    <h2 class="text-4xl font-bold text-center">Happy Customers</h2>
-    <p class="pt-6 pb-8 text-base max-w-2xl text-center m-auto">
+    <div><div className="w-full px-4 pt-16 pb-16" id="faq">
+    <h2 className="text-4xl font-bold text-center">Happy Customers</h2>
+    <p className="pt-6 pb-8 text-base max-w-2xl text-center m-auto">
         Lorem ipsum dolor sit amet
         consectetur adipisicing elit nam maxime quas fugiat tempore blanditiis, eveniet quia accusantium.
     </p>
     <div
-        class="mx-auto w-full max-w-4xl justify-center items-center grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-4">
+        className="mx-auto w-full max-w-4xl justify-center items-center grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-4">
         <Link  href="">
-            <Image width={200} height={200} alt="" class="h-28  mx-auto" src={CustomerOne}/>
+            <Image width={200} height={200} alt="" className="h-28  mx-auto" src={CustomerOne}/>
         </Link>
       
         <Link  href="">
-            <Image width={200} height={200} alt="" class="h-28  mx-auto" src={CustomerOne}/>
+            <Image width={200} height={200} alt="" className="h-28  mx-auto" src={CustomerOne}/>
         </Link>
       
         <Link  href="">
-            <Image width={200} height={200} alt="" class="h-28  mx-auto" src={CustomerOne}/>
+            <Image width={200} height={200} alt="" className="h-28  mx-auto" src={CustomerOne}/>
         </Link>
       
         <Link  href="">
-            <Image width={200} height={200} alt="" class="h-28  mx-auto" src={CustomerOne}/>
+            <Image width={200} height={200} alt="" className="h-28  mx-auto" src={CustomerOne}/>
         </Link>
       
         <Link  href="">
-            <Image width={200} height={200} alt="" class="h-28  mx-auto" src={CustomerOne}/>
+            <Image width={200} height={200} alt="" className="h-28  mx-auto" src={CustomerOne}/>
         </Link>
       
         <Link  href="">
-            <Image width={200} height={200} alt="" class="h-28  mx-auto" src={CustomerOne}/>
+            <Image width={200} height={200} alt="" className="h-28  mx-auto" src={CustomerOne}/>
         </Link>
       
     </div>
