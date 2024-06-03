23820db3d01c6c3993e686f543342db2160efab6
Homayoun <homayoun763@gmail.com>
2024-06-03 19:43:11 +0330

    Improve delete btn and add favicon

--- app/components/DeleteBlock.jsx
+++ app/components/DeleteBlock.jsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MdDeleteOutline } from "react-icons/md";
 import { useRouter } from "next/navigation";
 
@@ -7,22 +7,28 @@ const DeleteBlock = ({ path, id }) => {
   const [isConfirmOpen, setIsConfirmOpen] = useState(false);
   const router = useRouter();
 
-  const handleDelete = async () => {
-    const res = await fetch(`/api/${path}/${id}`, {
-      method: "DELETE",
-    });
-    setIsConfirmOpen(false);
-    if (res.ok) {
-      router.refresh();
-    } else {
-      console.error("Error deleting item:", res.statusText);
+  useEffect(() => {
+    if (!isConfirmOpen) {
+      return;
     }
+    const handleDelete = async () => {
+      try {
+        const res = await fetch(`/api/${path}/${id}`, {
+          method: "DELETE",
+        });
+        setIsConfirmOpen(false);
+        if (res.ok) {
+          router.refresh();
+        } else {
+          console.error("Error deleting item:", res.statusText);
+        }
+      } catch (error) {
+        console.error("Error deleting item:", error);
+      }
+    };
 
-  };
-
-  const handleConfirmClose = () => {
-    setIsConfirmOpen(false);
-  };
+    handleDelete();
+  }, [isConfirmOpen, path, id, router]);
 
   return (
     <>
@@ -34,34 +40,36 @@ const DeleteBlock = ({ path, id }) => {
         <MdDeleteOutline />
       </button>
 
-      {isConfirmOpen && ( 
+      {isConfirmOpen && (
         <div className="fixed z-10 inset-0 overflow-y-auto bg-gray-400 bg-opacity-75 p-4 md:p-8">
           <div className="w-full max-w-sm mx-auto bg-white rounded-lg shadow-md">
             <div className="flex justify-between items-center p-5 border-b border-gray-200">
               <h5 className="text-xl font-medium text-gray-800">Delete</h5>
               <button
                 type="button"
-                onClick={handleConfirmClose}
-                className="icon btn-close focus:outline-none"
-              ></button>
+                onClick={() => setIsConfirmOpen(false)}
+                className="focus:outline-none"
+              >
+                &times;
+              </button>
             </div>
             <div className="p-3 text-gray-700">
-            Are you sure you want to delete this item?
+              Are you sure you want to delete this item?
             </div>
             <div className="flex justify-end items-center p-3">
               <button
                 type="button"
-                onClick={handleConfirmClose}
+                onClick={() => setIsConfirmOpen(false)}
                 className="text-gray-500 hover:text-gray-700 focus:outline-none rounded-md border border-gray-200 p-2 mr-2"
               >
                 Cancel
               </button>
               <button
                 type="button"
-                onClick={handleDelete}
-                className="text-white bg-red-500 hover:bg-red-600 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+                onClick={() => setIsConfirmOpen(true)}
+                className="text-white bg-red-500 hover:bg-red-600 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5"
               >
-                 Delete
+                Delete
               </button>
             </div>
           </div>
@@ -72,4 +80,3 @@ const DeleteBlock = ({ path, id }) => {
 };
 
 export default DeleteBlock;
-
