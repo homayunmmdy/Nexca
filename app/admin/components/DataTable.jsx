0e4d599d2c898777740e9a6c5dcee4003b06430d
Homayoun <homayoun763@gmail.com>
2024-07-15 23:43:32 +0330

    sort DataTabe by id

--- app/admin/components/DataTable.jsx
+++ app/admin/components/DataTable.jsx
@@ -1,29 +1,34 @@
 import DeleteBlock from "@/app/components/DeleteBlock";
 import React from "react";
 
-const DataTable = ({ data , path }) => (
-    <div className="overflow-x-auto">
-        <table className="table table-zebra">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>sec</th>
-                    <th>delete</th>
-                </tr>
-            </thead>
-            {data?.map((item) => (
-                <tbody key={item.id}>
+const DataTable = ({ data, path }) => {
+    // Sort data by `secid` in ascending order
+    const sortedData = data ? [...data].sort((a, b) => a.secid - b.secid) : [];
+
+    return (
+        <div className="overflow-x-auto">
+            <table className="table table-zebra">
+                <thead>
                     <tr>
-                        <td>{item.secid}</td>
-                        <td>{item.name}</td>
-                        <td>
-                            <DeleteBlock path={path} id={item._id} />
-                        </td>
+                        <th>id</th>
+                        <th>sec</th>
+                        <th>delete</th>
                     </tr>
+                </thead>
+                <tbody>
+                    {sortedData.map((item) => (
+                        <tr key={item.id}>
+                            <td>{item.secid}</td>
+                            <td>{item.name}</td>
+                            <td>
+                                <DeleteBlock path={path} id={item._id} />
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
-            ))}
-        </table>
-    </div>
-);
+            </table>
+        </div>
+    );
+};
 
 export default DataTable;
