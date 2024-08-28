21fb7a86b24c7d3ffc6d13fd126c05d43b90401c
Homayoun <homayoun763@gmail.com>
2024-08-28 20:53:08 +0330

    Add : hooks, cash , util

---
+++ src/hooks/useGetSection.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { useState, useEffect } from "react";
+
+//@ts-ignore
+const useGetSection = (url, lengthItem, secId) => {
+    const [postData, setPostData] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchPostData = async () => {
+            try {
+                setLoading(true);
+                const response = await fetch(url);
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const data = await response.json();
+                setPostData(data.data);
+                setLoading(false);
+            } catch (error) {
+                console.error("Error fetching postData:", error);
+                setLoading(false);
+            }
+        };
+
+        fetchPostData();
+    }, [url]);
+
+    //@ts-ignore
+    const filteredData = postData?.filter((item) => item.section === `${secId}`);
+    const data = filteredData?.slice(lengthItem);
+
+    return { data, loading };
+};
+
+export default useGetSection;
