7aeb9b1caafe41ba06c82ce638535ec7a4194fd9
Homayoun <homayoun763@gmail.com>
2024-11-03 22:47:49 +0330

    add:  more feature to rich text editor

--- src/app/(pages)/admin/components/TiptapEditor.tsx
+++ src/app/(pages)/admin/components/TiptapEditor.tsx
@@ -1,8 +1,20 @@
+import "@/app/tiptap.css";
 import { Button } from "@/components";
+import Highlight from "@tiptap/extension-highlight";
+import Underline from "@tiptap/extension-underline";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import React from "react";
-import { MdFormatItalic, MdOutlineFormatBold } from "react-icons/md";
+import { GrOrderedList } from "react-icons/gr";
+import {
+  MdFormatItalic,
+  MdFormatListBulleted,
+  MdFormatQuote,
+  MdFormatUnderlined,
+  MdOutlineFormatBold,
+  MdStrikethroughS,
+} from "react-icons/md";
+import { PiHighlighterFill } from "react-icons/pi";
 
 interface TiptapEditorProps {
   content: string;
@@ -11,7 +23,11 @@ interface TiptapEditorProps {
 
 const TiptapEditor: React.FC<TiptapEditorProps> = ({ content, onChange }) => {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      Underline,
+      Highlight.configure({ multicolor: true }),
+    ],
     content,
     onUpdate: ({ editor }) => {
       onChange(editor.getHTML());
@@ -23,8 +39,37 @@ const TiptapEditor: React.FC<TiptapEditorProps> = ({ content, onChange }) => {
   }
 
   return (
-    <div className="rounded-md border border-gray-300 p-2">
-      <div className="mb-2 flex gap-2">
+    <div className="rounded-md border border-indigo-500 p-2">
+      <div className="mb-2 flex flex-wrap gap-2 px-3">
+        <Button
+          title={"H1"}
+          type="button"
+          color="btn-ghost"
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 2 }).run()
+          }
+          style={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}
+        />
+        <Button
+          title={"H2"}
+          type="button"
+          color="btn-ghost"
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 2 }).run()
+          }
+          style={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}
+        />
+
+        <Button
+          title={"H3"}
+          type="button"
+          color="btn-ghost"
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 3 }).run()
+          }
+          style={editor.isActive("heading", { level: 3 }) ? "is-active" : ""}
+        />
+
         <Button
           onClick={() => editor.chain().focus().toggleBold().run()}
           title={<MdOutlineFormatBold size={20} />}
@@ -34,11 +79,56 @@ const TiptapEditor: React.FC<TiptapEditorProps> = ({ content, onChange }) => {
         />
 
         <Button
+          title={<MdStrikethroughS size={20} />}
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          type="button"
+          color="btn-ghost"
+          style={editor.isActive("strike") ? "is-active" : ""}
+        />
+
+        <Button
+          title={<MdFormatQuote size={20} />}
+          type="button"
+          color="btn-ghost"
+          onClick={() => editor.chain().focus().toggleBlockquote().run()}
+          style={editor.isActive("blockquote") ? "is-active" : ""}
+        />
+
+        <Button
+          title={<MdFormatUnderlined size={20} />}
+          type="button"
+          color="btn-ghost"
+          onClick={() => editor.chain().focus().toggleUnderline().run()}
+          style={editor.isActive("underline") ? "is-active" : ""}
+        />
+
+        <Button
           onClick={() => editor.chain().focus().toggleItalic().run()}
           title={<MdFormatItalic size={20} />}
           type="button"
           color="btn-ghost"
           style={editor.isActive("italic") ? "is-active" : ""}
+        />
+        <Button
+          title={<GrOrderedList size={20} />}
+          type="button"
+          color="btn-ghost"
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          style={editor.isActive("orderedList") ? "is-active" : ""}
+        />
+        <Button
+          title={<MdFormatListBulleted size={20} />}
+          type="button"
+          color="btn-ghost"
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          style={editor.isActive("bulletList") ? "is-active" : ""}
+        />
+        <Button
+          title={<PiHighlighterFill size={20} />}
+          type="button"
+          color="btn-ghost"
+          onClick={() => editor.chain().focus().toggleHighlight().run()}
+          style={editor.isActive("highlight") ? "is-active" : ""}
         />
       </div>
       <EditorContent editor={editor} className="prose max-w-none" />
