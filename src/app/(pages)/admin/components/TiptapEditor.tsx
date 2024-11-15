9c2128bfef32f2920700c7e81259116fa02d572c
Homayoun <homayoun763@gmail.com>
2024-11-15 22:53:05 +0330

    add : image into rich text editor

--- src/app/(pages)/admin/components/TiptapEditor.tsx
+++ src/app/(pages)/admin/components/TiptapEditor.tsx
@@ -2,12 +2,14 @@ import "@/app/tiptap.css";
 import { Button } from "@/components";
 import CodeBlock from "@tiptap/extension-code-block";
 import Highlight from "@tiptap/extension-highlight";
+import Image from "@tiptap/extension-image";
 import Link from "@tiptap/extension-link";
 import Underline from "@tiptap/extension-underline";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import React, { useCallback } from "react";
 import { GrOrderedList } from "react-icons/gr";
+
 import { IoMdCode, IoMdLink } from "react-icons/io";
 import {
   MdFormatItalic,
@@ -16,8 +18,10 @@ import {
   MdFormatUnderlined,
   MdLinkOff,
   MdOutlineFormatBold,
+  MdOutlineImage,
   MdStrikethroughS,
 } from "react-icons/md";
+import { PiHighlighterFill } from "react-icons/pi";
 
 interface TiptapEditorProps {
   content: string;
@@ -30,6 +34,7 @@ const TiptapEditor: React.FC<TiptapEditorProps> = ({ content, onChange }) => {
       StarterKit,
       Underline,
       CodeBlock,
+      Image,
       Highlight.configure({ multicolor: true }),
       Link.configure({
         openOnClick: false,
@@ -68,6 +73,14 @@ const TiptapEditor: React.FC<TiptapEditorProps> = ({ content, onChange }) => {
       .run();
   }, [editor]);
 
+  const addImage = useCallback(() => {
+    const url = window.prompt("URL");
+
+    if (url) {
+      editor?.chain().focus().setImage({ src: url }).run();
+    }
+  }, [editor]);
+
   if (!editor) {
     return null;
   }
@@ -82,7 +95,9 @@ const TiptapEditor: React.FC<TiptapEditorProps> = ({ content, onChange }) => {
             editor.chain().focus().toggleHeading({ level: 2 }).run()
           }
           style={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}
-        >H1</Button>
+        >
+          H1
+        </Button>
         <Button
           type="button"
           color="btn-ghost"
@@ -90,7 +105,9 @@ const TiptapEditor: React.FC<TiptapEditorProps> = ({ content, onChange }) => {
             editor.chain().focus().toggleHeading({ level: 2 }).run()
           }
           style={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}
-        >H2</Button>
+        >
+          H2
+        </Button>
 
         <Button
           type="button"
@@ -99,28 +116,36 @@ const TiptapEditor: React.FC<TiptapEditorProps> = ({ content, onChange }) => {
             editor.chain().focus().toggleHeading({ level: 3 }).run()
           }
           style={editor.isActive("heading", { level: 3 }) ? "is-active" : ""}
-        >H3</Button>
+        >
+          H3
+        </Button>
 
         <Button
           onClick={() => editor.chain().focus().toggleBold().run()}
           type="button"
           color="btn-ghost"
           style={editor.isActive("bold") ? "is-active" : ""}
-        ><MdOutlineFormatBold size={20} /></Button>
+        >
+          <MdOutlineFormatBold size={20} />
+        </Button>
 
         <Button
           onClick={() => editor.chain().focus().toggleStrike().run()}
           type="button"
           color="btn-ghost"
           style={editor.isActive("strike") ? "is-active" : ""}
-        ><MdStrikethroughS size={20} /></Button>
+        >
+          <MdStrikethroughS size={20} />
+        </Button>
 
         <Button
           type="button"
           color="btn-ghost"
           onClick={() => editor.chain().focus().toggleBlockquote().run()}
           style={editor.isActive("blockquote") ? "is-active" : ""}
-        ><MdFormatQuote size={20} /></Button>
+        >
+          <MdFormatQuote size={20} />
+        </Button>
         <div className="control-group">
           <div className="button-group">
             <Button
@@ -128,13 +153,17 @@ const TiptapEditor: React.FC<TiptapEditorProps> = ({ content, onChange }) => {
               color="btn-ghost"
               onClick={setLink}
               style={editor.isActive("link") ? "is-active" : ""}
-            ><IoMdLink size={20} /></Button>
+            >
+              <IoMdLink size={20} />
+            </Button>
             <Button
               type="button"
               color="btn-ghost"
               onClick={() => editor.chain().focus().unsetLink().run()}
               disabled={!editor.isActive("link")}
-            ><MdLinkOff size={20} /></Button>
+            >
+              <MdLinkOff size={20} />
+            </Button>
           </div>
         </div>
         <Button
@@ -142,33 +171,54 @@ const TiptapEditor: React.FC<TiptapEditorProps> = ({ content, onChange }) => {
           color="btn-ghost"
           onClick={() => editor.chain().focus().toggleUnderline().run()}
           style={editor.isActive("underline") ? "is-active" : ""}
-        ><MdFormatUnderlined size={20} /></Button>
+        >
+          <MdFormatUnderlined size={20} />
+        </Button>
 
         <Button
           onClick={() => editor.chain().focus().toggleItalic().run()}
           type="button"
           color="btn-ghost"
           style={editor.isActive("italic") ? "is-active" : ""}
-        ><MdFormatItalic size={20} /></Button>
+        >
+          <MdFormatItalic size={20} />
+        </Button>
         <Button
           type="button"
           color="btn-ghost"
           onClick={() => editor.chain().focus().toggleOrderedList().run()}
           style={editor.isActive("orderedList") ? "is-active" : ""}
-        ><GrOrderedList size={20} /></Button>
+        >
+          <GrOrderedList size={20} />
+        </Button>
+
         <Button
           type="button"
           color="btn-ghost"
           onClick={() => editor.chain().focus().toggleBulletList().run()}
           style={editor.isActive("bulletList") ? "is-active" : ""}
-        ><MdFormatListBulleted size={20} /></Button>
-
+        >
+          <MdFormatListBulleted size={20} />
+        </Button>
+        <Button
+          type="button"
+          color="btn-ghost"
+          onClick={() => editor.chain().focus().toggleHighlight().run()}
+          style={editor.isActive("highlight") ? "is-active" : ""}
+        >
+          <PiHighlighterFill size={20} />
+        </Button>
         <Button
           type="button"
           color="btn-ghost"
           onClick={() => editor.chain().focus().toggleCodeBlock().run()}
           style={editor.isActive("codeBlock") ? "is-active" : ""}
-        ><IoMdCode size={20} /></Button>
+        >
+          <IoMdCode size={20} />
+        </Button>
+        <Button type="button" color="btn-ghost" onClick={addImage}>
+          <MdOutlineImage size={20} />
+        </Button>
       </div>
       <EditorContent editor={editor} className="prose max-w-none" />
     </div>
