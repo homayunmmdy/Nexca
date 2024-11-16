cd88e7793b81414785cd5e458d321718d93da4ac
Homayoun <homayoun763@gmail.com>
2024-11-16 23:37:15 +0330

    add loading for tickets admin

--- src/app/(pages)/admin/tickets/page.tsx
+++ src/app/(pages)/admin/tickets/page.tsx
@@ -6,13 +6,20 @@ import useFetch from "@/hooks/useFetch";
 import { TicketsCashType } from "@/types/CashTypes";
 import { checkMaster } from "@/util/Util";
 import React from "react";
+import TicketsSkeleton from "./ticketsSkeleton";
 
 const AdminTicketsPage: React.FC = () => {
-  const { data: tickets } = useFetch(TICKETS_QUERY_KEY, TICKETS_API_URL);
+  const { data: tickets, loading } = useFetch(
+    TICKETS_QUERY_KEY,
+    TICKETS_API_URL
+  );
   let master = checkMaster();
+  if (loading) {
+    return <TicketsSkeleton />;
+  }
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3">
         {tickets?.map((ticket: TicketsCashType) => (
           <TicketCard ticket={ticket} master={master} />
         ))}
