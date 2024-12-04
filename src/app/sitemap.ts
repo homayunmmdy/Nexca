488ed52abcd76030e144be2f1d6f0b2832041b88
Homayoun <129702378+homayunmmdy@users.noreply.github.com>
2024-12-04 12:12:42 +0330

    add: more pages into sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,21 +4,18 @@ export default function sitemap() {
   return [
     {
       url: SiteConfig.siteURL,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1,
     },
     {
       url: `${SiteConfig.siteURL}/demo`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
     },
     {
+      url: `${SiteConfig.siteURL}/services/1`,
+    },
+     {
       url: `${SiteConfig.siteURL}/about`,
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.5,
+    },
+    {
+      url: `${SiteConfig.siteURL}/contacts`,
     },
   ];
 }
