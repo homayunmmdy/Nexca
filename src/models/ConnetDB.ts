36c6b9f6990f4fe18de1b7c5f11b54af7e4e127b
Homayoun <homayoun763@gmail.com>
2025-01-11 22:36:33 +0330

    fix : CI for db connection

--- src/models/ConnetDB.ts
+++ src/models/ConnetDB.ts
@@ -4,17 +4,11 @@ import mongoose from "mongoose";
  * The MongoDB connection URI retrieved from the environment variables.
  * @type {string}
  */
-const MONGODB_URI = process.env.MONDOGDB as string;
-
-// Check if the MONGODB_URI environment variable is defined
-if (!MONGODB_URI) {
-  throw new Error("Please define the MONGODB_URI environment variable");
-}
 
 /**
  * Connects to the MongoDB database using the provided URI.
  * Also sets Mongoose's Promise implementation to use the global Promise object.
  * @throws {Error} If the connection to the database fails.
  */
-mongoose.connect(MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI as string);
 mongoose.Promise = global.Promise;
