ee7f5666a047069983d5d3afdb54d90353d9340f
Homayoun <homayoun763@gmail.com>
2025-01-11 23:49:52 +0330

    test : CopyRight #125

--- src/components/Footer/CopyRight.tsx
+++ src/components/Footer/CopyRight.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link";
+import React from "react";
 
-const CopyRight = ({ siteName }: { siteName: string }) => {
+interface Props {
+  siteName: string;
+}
+
+const CopyRight: React.FC<Props> = ({ siteName }: Props) => {
   return (
     <div className="py-4">
       <div className="flex items-center justify-center">
-        <span className="text-sm">
+        <span className="text-sm" data-testid='copyrightText'>
           Copyright@{new Date().getFullYear()} All Right Reserved by{" "}
-          <Link href="/">{siteName}</Link>
+          <Link href="/" title={siteName}>{siteName}</Link>
         </span>
       </div>
     </div>
