a972d562dfb151b9abac08b30dd466341911f87b
Homayoun <homayoun763@gmail.com>
2025-01-17 18:56:54 +0330

    add eslitn.json

--- src/app/(pages)/admin/tickets/page.tsx
+++ src/app/(pages)/admin/tickets/page.tsx
@@ -21,7 +21,7 @@ const AdminTicketsPage: React.FC = () => {
     <>
       <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3">
         {tickets?.map((ticket: TicketsCashType) => (
-          <TicketCard ticket={ticket} master={master} />
+          <TicketCard ticket={ticket} key={ticket._id} master={master} />
         ))}
       </div>
     </>
