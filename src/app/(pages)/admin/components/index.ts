476f728ad3c0429240ee5bcab7a5dac593e05662
Homayoun <homayoun763@gmail.com>
2025-01-20 19:38:51 +0330

    remove : EditForm and models #131

--- src/app/(pages)/admin/components/index.ts
+++ src/app/(pages)/admin/components/index.ts
@@ -2,6 +2,3 @@ export { default as EditPostForm } from "./forms/EditPostForm";
 export { default as EditSectionForm } from "./forms/EditSectionForm";
 export { default as EditServicesForm } from "./forms/EditServicesForm";
 export { default as EditTextAdvForm } from "./forms/EditTextAdvForm";
-export { default as EditTicketForm } from "./forms/EditTicketForm";
-export { default as EditTimeLineForm } from "./forms/EditTimeLineForm";
-export { default as EditContributionForm } from "./forms/EditContributionForm";
--- src/models/index.ts
+++ src/models/index.ts
@@ -5,11 +5,8 @@
 
 export { default as CommentsModel } from "./model/Comments";
 export { default as ContactsModel } from "./model/Contacts";
-export { default as ContributionModel } from "./model/Contribution";
 export { default as EmailsModel } from "./model/Emails";
 export { default as PostModel } from "./model/Post";
 export { default as SectionModel } from "./model/Section";
 export { default as ServiceModel } from "./model/Services";
 export { default as TextAdvModel } from "./model/TextAdv";
-export { default as TicketModel } from "./model/Ticket";
-export { default as TimeLineModel } from "./model/TimeLine";
