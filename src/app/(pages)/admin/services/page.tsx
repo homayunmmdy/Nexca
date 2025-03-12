90935dd294bf5260b68b2aeab2a02d349e2a7714
Homayoun <homayoun763@gmail.com>
2025-03-12 20:22:18 +0330

    add : tooltip when service is diable

--- src/app/(pages)/admin/services/page.tsx
+++ src/app/(pages)/admin/services/page.tsx
@@ -1,25 +1,25 @@
 "use client";
 import { ErrorText, Spinner } from "@/components";
+import { Button } from "@/components/atoms";
 import { POST_API_URL, SERVICES_API_URL } from "@/config/apiConstants";
 import { ALL_POSTS_QUERY_KEY, SERVICES_QUERY_KEY } from "@/config/Constants";
 import useFetch from "@/hooks/useFetch";
-import React from "react";
-import { DataTable, DeleteBlock } from "../components/elements";
-import { CiEdit } from "react-icons/ci";
-import { Button } from "@/components/atoms";
-import Link from "next/link";
 import { PostsCashType } from "@/types/CashTypes";
+import Link from "next/link";
+import React from "react";
+import { CiEdit } from "react-icons/ci";
+import { DeleteBlock } from "../components/elements";
 
 const Services: React.FC = () => {
-  const { data, loading } = useFetch(
-    SERVICES_QUERY_KEY,
-    SERVICES_API_URL
+  const { data, loading } = useFetch(SERVICES_QUERY_KEY, SERVICES_API_URL);
+  const { data: posts, loading: loadingPost } = useFetch(
+    ALL_POSTS_QUERY_KEY,
+    POST_API_URL
   );
-  const { data: posts, loading : loadingPost } = useFetch(ALL_POSTS_QUERY_KEY, POST_API_URL);
   if (loading && loadingPost) {
     return <Spinner />;
   }
-  
+
   const sortedData = data ? [...data].sort((a, b) => a.secid - b.secid) : [];
   const postCounts = posts?.reduce((acc: any, post: PostsCashType) => {
     const serviceId = post.services;
@@ -29,58 +29,69 @@ const Services: React.FC = () => {
     return acc;
   }, {} as Record<string, number>);
 
-  
-  const editor = true
+  const editor = true;
   return (
     <>
       <div className="overflow-x-auto">
-      {editor ? (
-        <div className="flex w-full justify-center">
-          <Link href={`/admin/services/new`}>
-            <Button
-              color="btn-primary"
-              className="btn-outline m-3"
-            >{`New services`}</Button>
-          </Link>
-        </div>
-      ) : null}
-      {sortedData?.length === 0 ? (
-        <ErrorText>There are currently no services created.</ErrorText>
-      ) : (
-        <table className="table table-zebra my-2">
-          <thead>
-            <tr>
-              <th>id</th>
-              <th>services</th>
-              <th>posts</th>
-              <th>edit</th>
-              {editor ? <th>delete</th> : null}
-            </tr>
-          </thead>
-          <tbody>
-            {sortedData?.map((item) => (
-              <tr key={item.id}>
-                <td>{item.secid}</td>
-                <td>{item.name}</td>
-                <td>{postCounts?.[item.secid] || 0}</td>
-                <td>
-                  <Link href={`/admin/services/${item._id}`}>
-                    <Button color="btn-warning" className="mb-2 me-2">
-                      <CiEdit size={25} />
-                    </Button>
-                  </Link>
-                </td>
-                {editor ? (
-                  <td>
-                    <DeleteBlock path="services" disable={postCounts?.[item.secid] > 0 ? true : false} id={item._id} />
-                  </td>
-                ) : null}
+        {editor ? (
+          <div className="flex w-full justify-center">
+            <Link href={`/admin/services/new`}>
+              <Button
+                color="btn-primary"
+                className="btn-outline m-3"
+              >{`New services`}</Button>
+            </Link>
+          </div>
+        ) : null}
+        {sortedData?.length === 0 ? (
+          <ErrorText>There are currently no services created.</ErrorText>
+        ) : (
+          <table className="table table-zebra m-3">
+            <thead>
+              <tr>
+                <th>id</th>
+                <th>services</th>
+                <th>posts</th>
+                <th>edit</th>
+                {editor ? <th>delete</th> : null}
               </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </div>
+            </thead>
+            <tbody>
+              {sortedData?.map((item) => (
+                <tr key={item.id}>
+                  <td>{item.secid}</td>
+                  <td>{item.name}</td>
+                  <td>{postCounts?.[item.secid] || 0}</td>
+                  <td>
+                    <Link href={`/admin/services/${item._id}`}>
+                      <Button color="btn-warning" className="mb-2 me-2">
+                        <CiEdit size={25} />
+                      </Button>
+                    </Link>
+                  </td>
+                  {editor ? (
+                    <td>
+                      <div className="group relative h-12 w-12">
+                        <DeleteBlock
+                          path="services"
+                          disable={postCounts?.[item.secid] > 0 ? true : false}
+                          id={item._id}
+                        />
+                        {/* Tooltip */}
+                        <div className="invisible absolute bottom-full left-1/2 right-0 mb-2 w-32 -translate-x-1/2 transform rounded-md bg-black px-2 py-1 text-sm text-white group-hover:visible">
+                          {postCounts?.[item.secid] > 0
+                            ? "This service contains posts. To delete it, please transfer all posts to another service."
+                            : "Delete Service"}
+                        </div>
+                      </div>
+                    </td>
+                  ) : null}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
     </>
   );
 };
