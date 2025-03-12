e74b41f2097cdfb779ee87b5b7325543d2f93523
Homayoun <homayoun763@gmail.com>
2025-03-12 23:03:34 +0330

    fix : config issue in PAW

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,26 @@
-import withPWA from 'next-pwa';
+import nextPwa from "next-pwa";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = nextPwa({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+})({
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'http',
-        hostname: '**',
-        port: '',
-        pathname: '/**',
+        protocol: "http",
+        hostname: "**",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
-};
-
-export default withPWA({
-  ...nextConfig, // Spread your existing Next.js config
-  pwa: {
-    dest: 'public', // Destination directory for the PWA files
-    disable: process.env.NEXT_PUBLIC_STATUS === 'dev', // Disable PWA in development
-    register: true, // Register the PWA service worker
-    skipWaiting: true, // Skip waiting for service worker to activate
-  },
 });
+
+export default nextConfig;
