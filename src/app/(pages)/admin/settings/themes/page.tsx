12ec1647343a0e473f54f81726478a0aef4190b9
Homayoun <homayoun763@gmail.com>
2025-03-27 21:30:12 +0330

    show : list of theme in setting  theme page

--- src/app/(pages)/admin/settings/themes/page.tsx
+++ src/app/(pages)/admin/settings/themes/page.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+"use client";
+import { ThemeFiled } from "@/components/atoms";
+import { DARKTHEME } from "@/config/Constants";
+import { ThemesConfig } from "@/config/themes";
+import { useEffect, useState } from "react";
 
 const ThemeSettingPage = () => {
-  return (
-    <div>ThemeSettingPage</div>
-  )
-}
+  const [theme, setTheme] = useState<string>(DARKTHEME);
 
-export default ThemeSettingPage
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      setTheme(savedTheme);
+      document.documentElement.setAttribute("data-theme", savedTheme);
+    }
+  }, []);
+  return (
+    <ul
+      tabIndex={0}
+      className="grid w-full p-2 grid-cols-2 md:grid-cols-3 md:gap-3 xl:grid-cols-4 xl:gap-4"
+    >
+      {ThemesConfig.map((item, index) => (
+        <ThemeFiled
+          state={theme}
+          key={index}
+          setState={setTheme}
+          Theme={item.name}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ThemeSettingPage;
