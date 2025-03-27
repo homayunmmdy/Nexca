9f7261b6aef190fe75d35e15e79ca7028eeddd30
Homayoun <homayoun763@gmail.com>
2025-03-27 21:55:23 +0330

    add : 4 More themes into list of themes

--- src/config/themes.tsx
+++ src/config/themes.tsx
@@ -1,15 +1,23 @@
 import {
+  BUMBLEBEETHEME,
+  CORPORATETHEME,
   CUPCAKETHEME,
   DARKTHEME,
+  EMERALADTHEME,
   FORESTTHEME,
   LIGHTTHEME,
   LUXURYTHEME,
+  SYTHWAVETHEME,
 } from "./Constants";
 
 export const ThemesConfig = [
   { name: LIGHTTHEME, activate: true },
   { name: DARKTHEME, activate: true },
   { name: CUPCAKETHEME, activate: true },
+  { name: BUMBLEBEETHEME, activate: false },
+  { name: EMERALADTHEME, activate: false },
+  { name: CORPORATETHEME, activate: false },
+  { name: SYTHWAVETHEME, activate: false },
   { name: FORESTTHEME, activate: true },
   { name: LUXURYTHEME, activate: true },
 ];
