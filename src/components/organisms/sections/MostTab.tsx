75861abb731ff924aa3be637411cce1ed69172a2
Homayoun <homayoun763@gmail.com>
2025-04-04 00:18:23 +0330

    add : real data in latest posts

--- src/components/organisms/sections/MostTab.tsx
+++ src/components/organisms/sections/MostTab.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Button, SectionsTitr } from "@/components/atoms";
 import Link from "next/link";
 import { FaCircleDot } from "react-icons/fa6";
+import LatestLinearPost from "./LatestLinearPost";
 
 const MostTab = () => {
   const [activeTab, setActiveTab] = useState<'latest' | 'most'>('latest');
@@ -25,21 +26,7 @@ const MostTab = () => {
       </div>
       
       {activeTab === 'latest' && (
-        <ul className="mb-3 flex flex-col gap-4">
-          {[...Array(3)].map((_, index) => (
-            <li
-              key={index}
-              className="group flex cursor-pointer items-start gap-2 rounded-xl border-2 border-dotted border-gray-400 p-2 hover:border-solid hover:border-gray-900"
-            >
-              <div className="h-7 w-7 shrink-0">
-                <FaCircleDot className="h-full w-full text-indigo-700" size={28} color="#4338CA" />
-              </div>
-              <Link className="line-clamp-2 font-semibold group-hover:text-indigo-600" href="/">
-                Latest post {index + 1}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <LatestLinearPost />
       )}
       
       {activeTab === 'most' && (
