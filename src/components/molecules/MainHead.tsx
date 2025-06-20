5e347f03f100f73d33f8b7ee1beb733b86a693d1
Homayoun <homayoun763@gmail.com>
2025-06-20 16:13:15 +0330

    update : MainHead style

--- src/components/molecules/MainHead.tsx
+++ src/components/molecules/MainHead.tsx
@@ -11,14 +11,14 @@ interface Props {
 function MainHead({title, description, createdAt}: Props) {
     return (
         <div
-            className="relative mt-[-30px] overflow-hidden bg-gradient-to-r from-indigo-900 via-indigo-800 to-purple-900">
+            className="relative mt-[30px] overflow-hidden bg-gradient-to-r from-indigo-900 via-indigo-800 to-purple-900">
             <div className="absolute inset-0 bg-gradient-to-b from-black/20 to-transparent opacity-40"></div>
             <Container className="relative py-8">
                 <div
-                    className="text-4xl text-center font-bold tracking-tight mb-4 pb-[6px] text-white sm:text-5xl md:text-6xl line-clamp-1 md:line-clamp-2">
+                    className="text-4xl text-center font-bold tracking-tight mb-4  text-white sm:text-5xl md:text-6xl line-clamp-3">
                     {title}
                 </div>
-                <p className="bg-gradient-to-r from-indigo-400 to-purple-300 bg-clip-text text-transparent line-clamp-2 text-center md:line-clamp-3">{description}</p>
+                <p className="bg-gradient-to-r from-indigo-400 to-purple-300 bg-clip-text text-transparent line-clamp-4 text-center md:line-clamp-3">{description}</p>
                 {createdAt && (
                     <p className="my-3 text-center text-lg text-white">
                         <FormatTime timestamp={createdAt} />
