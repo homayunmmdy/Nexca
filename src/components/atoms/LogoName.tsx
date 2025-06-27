9070d146a12d68d7b52c596ee5be2f22b396ee42
Homayoun <homayoun763@gmail.com>
2025-06-27 15:00:02 +0330

    test: LogoName

--- src/components/atoms/LogoName.tsx
+++ src/components/atoms/LogoName.tsx
@@ -1,16 +1,17 @@
 "use client";
+import React from 'react';
 import SiteConfig from "../../config/site";
 import { motion } from "framer-motion";
 import Link from "next/link";
 
-const LogoName = () => {
+const LogoName: React.FC = () => {
   return (
     <motion.p
       className="text-xl font-bold text-indigo-700 md:text-2xl lg:text-3xl"
       whileHover={{ scale: 1.05 }}
       whileTap={{ scale: 0.95 }}
     >
-    <Link href="/" title={SiteConfig.name}>
+    <Link href="/" title={SiteConfig.name} aria-label="logo link">
         {SiteConfig.name}
       </Link>
     </motion.p>
