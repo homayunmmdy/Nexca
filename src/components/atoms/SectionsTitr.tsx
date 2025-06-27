7f5988a770363abaa5da51a50003031c5a32d8b8
Homayoun <homayoun763@gmail.com>
2025-06-27 20:01:30 +0330

    test: SectionsTitr

--- src/components/atoms/SectionsTitr.tsx
+++ src/components/atoms/SectionsTitr.tsx
@@ -1,15 +1,17 @@
+import React from 'react';
+
 interface SectionsTitrProps {
     children: string,
     bg? : string,
     line? : boolean
 }
-const SectionsTitr = ({ children, bg = 'bg-indigo-700', line = true}: SectionsTitrProps) => {
+const SectionsTitr: React.FC<SectionsTitrProps> = ({ children, bg = 'bg-indigo-700', line = true}: SectionsTitrProps) => {
   return (
     <div className="flex items-center gap-2 mb-2 md:mb-4">
       <p className={`${bg} p-3 rounded-lg text-white text-nowrap text-sm md:text-lg`}>
         {children}
       </p>
-      {line === true && <div className="h-0.5 w-full bg-base-300"></div>}
+      {line === true && <div data-testid="line" className="h-0.5 w-full bg-base-300"></div>}
     </div>
   );
 };
