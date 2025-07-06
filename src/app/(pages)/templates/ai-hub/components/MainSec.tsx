f07ef7330ad89e4512f636da77dc7055734e8d08
Homayoun <homayoun763@gmail.com>
2025-07-06 16:55:06 +0330

    add : sectionController to main section in ai-hub template

--- src/app/(pages)/templates/ai-hub/components/MainSec.tsx
+++ src/app/(pages)/templates/ai-hub/components/MainSec.tsx
@@ -2,17 +2,20 @@
 import { AIHUB_MAIN_QUERY_KEY } from "@/config/Constants";
 import useGetSection from "@/hooks/useGetSection";
 import { PostsCashType } from "@/types/CashTypes";
+import { SectionController } from "@/util/controller/sectionsController";
 import VerticalCard from "../../(components)/VerticalCard";
 
 function MainSec() {
   const { data } = useGetSection(AIHUB_MAIN_QUERY_KEY, -1, 9);
 
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-      {data?.map((item: PostsCashType) => (
-        <VerticalCard data={item} key={item._id} />
-      ))}
-    </div>
+    <SectionController sectionId={9}>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        {data?.map((item: PostsCashType) => (
+          <VerticalCard data={item} key={item._id} />
+        ))}
+      </div>
+    </SectionController>
   );
 }
 
