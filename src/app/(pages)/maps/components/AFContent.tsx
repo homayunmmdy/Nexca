9dae3379a0ca749e2c62955428f8775303309d3d
Homayoun <homayoun763@gmail.com>
2025-07-10 13:24:35 +0330

    add : notfound for each province

--- src/app/(pages)/maps/components/AFContent.tsx
+++ src/app/(pages)/maps/components/AFContent.tsx
@@ -2,42 +2,44 @@
 import React from 'react';
 import {AF_MAP_CONTENT} from "@/config/Constants";
 import useGetMapContent from "@/hooks/useGetMapContent";
-import {Spinner} from "@/components/atoms";
+import {ErrorText, Spinner} from "@/components/atoms";
 import {MapsCashType} from "@/types/CashTypes";
 import Image from "next/image";
 
 interface Props {
-    provinceID : number,
+    provinceID: number,
     countryID?: number
 }
 
-const AfContent = ({provinceID, countryID = 1} : Props) => {
-    const {data,loading} = useGetMapContent(AF_MAP_CONTENT, -1, countryID, provinceID);
+const AfContent = ({provinceID, countryID = 1}: Props) => {
+    const {data, loading} = useGetMapContent(AF_MAP_CONTENT, -1, countryID, provinceID);
 
-    if(loading) {
-        return <Spinner />
+    if (loading) {
+        return <Spinner/>
     }
     return (
         <div className='grid grid-cols-2 gap-4'>
-            {data?.map((content : MapsCashType) => (
-                <div key={content._id} className="card bg-base-100 w-full shadow-sm">
-                    <figure>
-                        <Image
-                            width={290}
-                            height={171}
-                            src={content.imgurl}
-                            title={content.title}
-                            alt={content.title} />
-                    </figure>
-                    <div className="card-body">
-                        <h2 className="card-title line-clamp-2">
-                            {content.title}
-                            <div className="badge badge-secondary">NEW</div>
-                        </h2>
-                        <p className='line-clamp-3'>{content.description}</p>
+            {data.length > 0 ? (
+                data?.map((content: MapsCashType) => (
+                    <div key={content._id} className="card bg-base-100 w-full shadow-sm">
+                        <figure>
+                            <Image
+                                width={290}
+                                height={171}
+                                src={content.imgurl}
+                                title={content.title}
+                                alt={content.title}/>
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title line-clamp-2">
+                                {content.title}
+                                <div className="badge badge-secondary">NEW</div>
+                            </h2>
+                            <p className='line-clamp-3'>{content.description}</p>
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))
+            ) : <ErrorText>No Content found for this province.</ErrorText>}
         </div>
     );
 };
