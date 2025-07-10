819cdd54eb7d9a2fe84be275612ef823649de8b2
Homayoun <homayoun763@gmail.com>
2025-07-10 12:55:40 +0330

    add : list of all provinces in map file

--- src/config/maps.tsx
+++ src/config/maps.tsx
@@ -1,6 +1,138 @@
 export const AF_MAP_DATA = [
     {
-        secid : '1',
-        name : 'Badakhshan'
+        secid: '1',
+        name: 'Badakhshan'
+    },
+    {
+        secid: '2',
+        name: 'Badghis'
+    },
+    {
+        secid: '3',
+        name: 'Baghlan'
+    },
+    {
+        secid: '4',
+        name: 'Balkh'
+    },
+    {
+        secid: '5',
+        name: 'Bamyan'
+    },
+    {
+        secid: '6',
+        name: 'Daykundi'
+    },
+    {
+        secid: '7',
+        name: 'Farah'
+    },
+    {
+        secid: '8',
+        name: 'Faryab'
+    },
+    {
+        secid: '9',
+        name: 'Ghazni'
+    },
+    {
+        secid: '10',
+        name: 'Ghor'
+    },
+    {
+        secid: '11',
+        name: 'Helmand'
+    },
+    {
+        secid: '12',
+        name: 'Herat'
+    },
+    {
+        secid: '13',
+        name: 'Jowzjan'
+    },
+    {
+        secid: '14',
+        name: 'Kabul'
+    },
+    {
+        secid: '15',
+        name: 'Kandahar'
+    },
+    {
+        secid: '16',
+        name: 'Kapisa'
+    },
+    {
+        secid: '17',
+        name: 'Khost'
+    },
+    {
+        secid: '18',
+        name: 'Kunar'
+    },
+    {
+        secid: '19',
+        name: 'Kunduz'
+    },
+    {
+        secid: '20',
+        name: 'Laghman'
+    },
+    {
+        secid: '21',
+        name: 'Logar'
+    },
+    {
+        secid: '22',
+        name: 'Nangarhar'
+    },
+    {
+        secid: '23',
+        name: 'Nimruz'
+    },
+    {
+        secid: '24',
+        name: 'Nuristan'
+    },
+    {
+        secid: '25',
+        name: 'Paktia'
+    },
+    {
+        secid: '26',
+        name: 'Paktika'
+    },
+    {
+        secid: '27',
+        name: 'Panjshir'
+    },
+    {
+        secid: '28',
+        name: 'Parwan'
+    },
+    {
+        secid: '29',
+        name: 'Samangan'
+    },
+    {
+        secid: '30',
+        name: 'Sar-e Pol'
+    },
+    {
+        secid: '31',
+        name: 'Takhar'
+    },
+    {
+        secid: '32',
+        name: 'Urozgan'
+    },
+    {
+        secid: '33',
+        name: 'Wardak'
+    },
+    {
+        secid: '34',
+        name: 'Zabul'
     }
-]
+];
