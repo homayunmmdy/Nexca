43b11aaa72dfc52dd7ff2d0503654008ce0725bd
Homayoun <homayoun763@gmail.com>
2025-07-17 14:00:43 +0330

    show : all children of  header

--- src/components/organisms/MobileMenu.tsx
+++ src/components/organisms/MobileMenu.tsx
@@ -1,6 +1,8 @@
+'use client'
 import { GlobalSearchInput } from "@/components/molecules";
 import { HeaderMenuConfigType } from "@/types/entities";
 import Link from "next/link";
+import { useState } from "react";
 
 const MobileMenu = ({
   pathname,
@@ -9,9 +11,15 @@ const MobileMenu = ({
   pathname: string;
   nav: HeaderMenuConfigType[];
 }) => {
+   const [activeId, setActiveId] = useState<string | null>(null);
+
+  const toggleItem = (id: string) => {
+    setActiveId(activeId === id ? null : id);
+  };
+
   return (
     <nav className="fixed left-0 bg-base-200 top-[80px] z-50 mx-auto w-[90%] sm:w-[70%] md:w-[60%] h-full border-2 border-indigo-700 lg:hidden">
-      <ul className="my-3 flex flex-col items-center gap-4 bg-inherit p-2  lg:hidden">
+      <ul className="my-3 h-screen overflow-y-auto flex flex-col items-center gap-4 bg-inherit p-2  lg:hidden">
         <li
           className="flex items-center gap-3"
           key={`mobile_menu_item_starter`}
@@ -38,6 +46,17 @@ const MobileMenu = ({
                 >
                   {item.name}
                 </Link>
+              )}
+              {item.children && (
+                <ul className="grid grid-cols-2 gap-4 mt-2 ">
+                    {item.children.map((nav) => (
+                      <li key={nav.id} className="rounded-xl border-2 border-indigo-700 p-2">
+                        <Link href={nav.href} title={nav.name}>
+                        {nav.name}
+                        </Link>
+                      </li>
+                    ))}
+                </ul>
               )}
             </li>
           );
