073c8068d1d250334da9bddad4e5052907452d49
Homayoun <homayoun763@gmail.com>
2025-07-23 19:39:17 +0330

    add : SE provinces into map

--- src/config/getProvinceData.tsx
+++ src/config/getProvinceData.tsx
@@ -1,17 +1,25 @@
 // Get the appropriate province data based on selected country
-import {AD_MAP_DATA, AF_MAP_DATA, AL_MAP_DATA, DZ_MAP_DATA} from "@/config/maps";
+import {
+  AD_MAP_DATA,
+  AF_MAP_DATA,
+  AL_MAP_DATA,
+  DZ_MAP_DATA,
+  SE_MAP_DATA,
+} from "@/config/maps";
 
 export const getProvinceData = (selectedService: string) => {
-    switch (selectedService) {
-        case '1': // Afghanistan
-            return AF_MAP_DATA;
-        case '2': // Albania
-            return AL_MAP_DATA;
-        case '3':
-            return DZ_MAP_DATA // Algeria
-        case '4':
-            return AD_MAP_DATA // Andorra
-        default:
-            return [];
-    }
+  switch (selectedService) {
+    case "1": // Afghanistan
+      return AF_MAP_DATA;
+    case "2": // Albania
+      return AL_MAP_DATA;
+    case "3":
+      return DZ_MAP_DATA; // Algeria
+    case "4":
+      return AD_MAP_DATA; // Andorra
+    case "160":
+      return SE_MAP_DATA; // Sweden
+    default:
+      return [];
+  }
 };
