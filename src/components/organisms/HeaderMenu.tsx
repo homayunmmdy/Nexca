9e6a35edcba6d8843c42569c6aff8d733557627c
Homayoun <homayoun763@gmail.com>
2025-08-13 12:29:55 +0330

    refactor : active style for links in mobile and desktop headers

--- src/components/organisms/HeaderMenu.tsx
+++ src/components/organisms/HeaderMenu.tsx
@@ -1,7 +1,8 @@
 import { HeaderMenuConfigType } from "@/types/entities";
+import classNames from "classnames";
 import Link from "next/link";
-import HeaderServices from "./HeaderServices";
 import HeaderMap from "./HeaderMap";
+import HeaderServices from "./HeaderServices";
 
 const HeaderMenu = ({
   pathname,
@@ -18,23 +19,17 @@ const HeaderMenu = ({
             key={`desktop_menu_item_${item.id}`}
             className="mx-1 group static xl:px-1 py-2"
           >
-            {pathname == item.link ? (
-              <Link
-                href={item.link}
-                title={item.name}
-                className="rounded-xl bg-indigo-700 text-white hover:bg-indigo-700"
-              >
-                {item.name}
-              </Link>
-            ) : (
-              <Link
-                className="rounded-xl border-2 border-base-100 hover:border-blue-700 hover:bg-base-100 hover:text-indigo-700"
-                href={item.link}
-                title={item.name}
-              >
-                {item.name}
-              </Link>
-            )}
+            <Link
+              href={item.link}
+              title={item.name}
+              className={classNames({
+                "rounded-xl border-2 border-base-100 hover:bg-indigo-700 hover:text-white transition-colors ease-in-out":
+                  true,
+                "bg-indigo-700 text-white": pathname == item.link,
+              })}
+            >
+              {item.name}
+            </Link>
             {item.children && (
               <ul className="absolute top-12 right-0 hidden group-hover:flex bg-base-300 rounded-xl p-2.5 w-full gap-2.5 flex-wrap">
                 {item.children.map((nav) => (
--- src/components/organisms/MobileMenu.tsx
+++ src/components/organisms/MobileMenu.tsx
@@ -1,6 +1,7 @@
 "use client";
-import {GitHubBtn, GlobalSearchInput} from "@/components/molecules";
+import { GitHubBtn } from "@/components/molecules";
 import { HeaderMenuConfigType } from "@/types/entities";
+import classNames from "classnames";
 import Link from "next/link";
 import { useState } from "react";
 
@@ -20,8 +21,6 @@ const MobileMenu = ({
   return (
     <nav className="fixed left-0 bg-base-200 top-[80px] z-50 mx-auto w-[90%] sm:w-[70%] md:w-[60%] h-full border-2 border-indigo-700 lg:hidden">
       <ul className="my-3 h-screen overflow-y-auto flex flex-col items-center gap-4 bg-inherit p-2  lg:hidden">
-
-
         {nav?.map((item) => {
           return (
             <li
@@ -29,23 +28,16 @@ const MobileMenu = ({
               onClick={() => item.children && toggleItem(item.id)}
               className="mx-1 w-3/4"
             >
-              {pathname === item.link ? (
-                <Link
+              <Link
                   href={item.link}
                   title={item.link}
-                  className="block w-full rounded-xl bg-indigo-700 p-2 text-center text-white hover:bg-indigo-700"
-                >
-                  {item.name}
-                </Link>
-              ) : (
-                <Link
-                  className="block w-full rounded-xl border-2 border-indigo-700 p-2 text-center"
-                  href={item.link}
-                  title={item.link}
-                >
-                  {item.name}
-                </Link>
-              )}
+                  className={classNames({
+                    'block w-full border-2 rounded-xl border-indigo-700 text-center p-2': true,
+                    'bg-indigo-700 text-white': pathname === item.link
+                  })}
+              >
+                {item.name}
+              </Link>
               {item.children && (
                 <ul
                   className={`grid grid-cols-2 gap-4 mt-2 transition-all duration-300 ease-in-out overflow-hidden 
