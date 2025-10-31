667876bb6e222bff8685eb75e8ab1d21d3616956
Homayoun <homayoun763@gmail.com>
2025-10-31 17:53:25 +0330

    add : loading spinner for the comments in edit mode

--- src/app/(features)/admin/(pages)/posts/[id]/comments/page.tsx
+++ src/app/(features)/admin/(pages)/posts/[id]/comments/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Comment from '@/app/(pages)/posts/[id]/components/Comment';
-import { ErrorText } from '@/components/atoms';
+import { ErrorText, Spinner } from '@/components/atoms';
 import { COMMENTS_API_URL } from '@/config/apiConstants';
 import { COMMENTS_KEY } from '@/config/Constants';
 import useFetch from '@/hooks/useFetch';
@@ -8,9 +8,13 @@ import { CommentsCashType } from '@/types/CashTypes';
 import { GetIdOfPost } from '@/util/Util';
 
 const PostCommentsPage = () => {
-   const { data } = useFetch(COMMENTS_KEY, COMMENTS_API_URL);
+   const { data, loading } = useFetch(COMMENTS_KEY, COMMENTS_API_URL);
    const postId = GetIdOfPost();
    const comments = data?.filter((item: any) => item.postId == postId);
+
+   if (loading) {
+      return <Spinner />;
+   }
    return (
       <div>
          {comments?.length > 0 ? (
